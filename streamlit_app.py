3ed73729a15bd86162b9760ab5473e5f1e397756
mwhitneyMOS <mwhitney@mos.org>
2026-03-19 12:20:08 -0400

    Update streamlit_app.py
    Added in SmoothieFroot API

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,7 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
         
+# new section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
